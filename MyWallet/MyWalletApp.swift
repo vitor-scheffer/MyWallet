4786812580d7737b7ca2b07b4a71805b991215bc
Vitor Scheffer <vitorscheffer61@gmail.com>
2024-07-08 13:13:24 -0300

    Initial Commit

---
+++ MyWallet/MyWalletApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWalletApp.swift
+//  MyWallet
+//
+//  Created by Vitor Scheffer on 08/07/24.
+//
+
+import SwiftUI
+
+@main
+struct MyWalletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
