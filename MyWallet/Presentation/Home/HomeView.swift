9b364631bd262a8de8cfd0ce0cf064b220278064
Vitor Scheffer <vitorscheffer61@gmail.com>
2024-07-09 00:25:40 -0300

    Implementa quoteUseCase e HomeViewModel

--- MyWallet/Presentation/Home/HomeView.swift
+++ MyWallet/Presentation/Home/HomeView.swift
@@ -8,11 +8,25 @@
 import SwiftUI
 
 struct HomeView: View {
+    @ObservedObject private var model: HomeViewModel
+    
+    init(model: HomeViewModel) {
+        self.model = model
+    }
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+#if DEBUG
 #Preview {
-    HomeView()
+    HomeView(model: HomeViewModel(
+        quotesUseCase: QuotesUseCase(
+            repository: QuoteListRepository(
+                apiService: CoreApi()
+            )
+        )
+    ))
 }
+#endif
